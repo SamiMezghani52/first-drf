00f6355bb73e2a47d28dd9c3b153c3c0fd604401
SamiMezghani52 <sami.mezghenni@gmail.com>
2021-08-13 22:27:04 +0100

    Last Commit

--- drf_project/drf_project/views.py
+++ drf_project/drf_project/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render
+from rest_framework.serializers import Serializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
+from drfapp.serializers import studentSerializer
+from drfapp.models import student
 
 class TestView(APIView):
+    permission_classes = (IsAuthenticated, )
+
     def get(self, request, *args, **kwargs):
-        data = {
-            'username': 'admin',
-            'years_active': 10,
-        }
-        return Response(data)
+        qs = student.objects.all()
+        student1 = qs.first()
+        serializer = studentSerializer(student1)
+        return Response(serializer.data)
+    
+    def post(self, request, *args, **kwargs):
+        serializer = studentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
